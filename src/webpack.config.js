01bdb3b65fca6f03d612e9342959b83b6ab4a0a0
Tom Dye <github@tomdye.co.uk>
2017-01-06 17:18:40 +0000

    working with no stylus

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -6,10 +6,8 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer-sunburst').BundleA
 const path = require('path');
 const basePath = process.cwd();
 const cssModuleIdent = '[name]__[local]__[hash:base64:5]';
-const stylusLoader = ExtractTextPlugin.extract([ 'css-loader?sourceMap', 'stylus-loader?sourceMap' ]);
-const stylusModuleLoader = ExtractTextPlugin.extract([
-	`css-loader?modules&sourceMap&localIdentName=${cssModuleIdent}`,
-	'stylus-loader?sourceMap'
+const cssModuleLoader = ExtractTextPlugin.extract([
+	`css-loader?modules&sourceMap&localIdentName=${cssModuleIdent}`
 ]);
 
 module.exports = function (args) {
@@ -41,7 +39,7 @@ module.exports = function (args) {
 	return {
 		entry: {
 			'src/main': [
-				path.join(basePath, 'src/main.styl'),
+				path.join(basePath, 'src/main.css'),
 				path.join(basePath, 'src/main.ts')
 			]
 		},
@@ -68,10 +66,9 @@ module.exports = function (args) {
 				{ test: /\.js?$/, loader: 'umd-compat-loader' },
 				{ test: /\.html$/, loader: 'html' },
 				{ test: /\.(jpe|jpg|png|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'file?name=[path][name].[hash:6].[ext]' },
-				{ test: /\.styl$/, exclude: /\.module\.styl$/, loader: stylusLoader },
-				{ test: /\.module\.styl$/, loader: stylusModuleLoader },
-				{ test: /\.module\.styl\.json$/, loader: 'json-css-module-loader' },
-				{ test: /\.module\.css$/, loader: ExtractTextPlugin.extract([ 'css-loader?sourceMap' ]) }
+				{ test: /src[\\\/].*\.css?$/, loader: cssModuleLoader },
+				{ test: /\.css$/, exclude: /src[\\\/].*/, loader: ExtractTextPlugin.extract([ 'css-loader?sourceMap' ]) },
+				{ test: /\.css\.json$/, exclude: /src[\\\/].*/, loader: 'json-css-module-loader' }
 			]
 		},
 		plugins: plugins,
@@ -81,3 +78,12 @@ module.exports = function (args) {
 		}
 	};
 }
+
+	// { test: /src[\\\/].*\.ts?$/, loader: 'umd-compat-loader!ts-loader' },
+	// { test: /\.js?$/, loader: 'umd-compat-loader' },
+	// { test: /\.html$/, loader: 'html' },
+	// { test: /\.(jpe|jpg|png|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'file?name=[path][name].[hash:6].[ext]' },
+	// { test: /\.styl$/, exclude: /\.module\.styl$/, loader: stylusLoader },
+	// { test: /\.module\.styl$/, loader: stylusModuleLoader },
+	// { test: /\.module\.styl\.json$/, loader: 'json-css-module-loader' },
+	// { test: /\.module\.css$/, loader: ExtractTextPlugin.extract([ 'css-loader?sourceMap' ]) }
